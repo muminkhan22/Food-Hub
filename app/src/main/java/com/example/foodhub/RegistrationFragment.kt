cd895b1515da92f3ccd7d675b938215c8458d5de
mdmum <mdmuminkhan84@gmail.com>
2025-03-16 02:54:59 +0600

    xml-digen

--- app/src/main/java/com/example/foodhub/RegistrationFragment.kt
+++ app/src/main/java/com/example/foodhub/RegistrationFragment.kt
@@ -1,70 +1,60 @@
 package com.example.foodhub
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+
 import android.widget.Toast
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.foodhub.databinding.FragmentRegistrationBinding
 
+class RegistrationFragment : BasteFragment<FragmentRegistrationBinding>
+    (FragmentRegistrationBinding::inflate) {
 
-
-
-class RegistrationFragment : Fragment() {
-    lateinit var binding: FragmentRegistrationBinding
     private val viewModel: RegistrationViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding= FragmentRegistrationBinding.inflate(inflater,container,false)
-        setlistener()
-        registrationObserver()
-        return binding.root
-    }
-    private fun setlistener() {
-        with(binding){
+    override fun setlistener() {
+        with(binding) {
             RrgisTV2.setOnClickListener {
                 userNam.isEmpty()
                 EmailTV1.isEmpty()
                 passTV1.isEmpty()
 
-                if (!userNam.isEmpty() && !EmailTV1.isEmpty() && !passTV1.isEmpty()){
-                   Toast.makeText(context,"All Input Done.!",Toast.LENGTH_LONG).show()
-                    val user= UserRegistr(
+                if (!userNam.isEmpty() && !EmailTV1.isEmpty() && !passTV1.isEmpty()) {
+                    Toast.makeText(context, "All Input Done.!", Toast.LENGTH_LONG).show()
+                    val user = UserRegistr(
                         userNam.text.toString(),
                         EmailTV1.text.toString(),
                         passTV1.text.toString(),
                         "seller",
                         ""
-                        )
+                    )
                     viewModel.userResistration(user)
                 }
             }
-
         }
     }
-    private fun registrationObserver() {
+
+    override fun allObserver() {
+        registrationObserver()
+    }
+
+    fun registrationObserver() {
         viewModel.reistrationRespons.observe(viewLifecycleOwner) {
-            when(it){
+            when (it) {
                 is DataState.Error<*> -> {
-                    Toast.makeText(context,it.massage, Toast.LENGTH_SHORT).show()
+                    loader.dismiss()
+                    Toast.makeText(context, it.massage, Toast.LENGTH_SHORT).show()
                 }
+
                 is DataState.Loding<*> -> {
-                    Toast.makeText(context,"Lodding...!", Toast.LENGTH_SHORT).show()
+                    loader.show()
+                    Toast.makeText(context, "Lodding...!", Toast.LENGTH_SHORT).show()
                 }
+
                 is DataState.Suscess<*> -> {
-                    Toast.makeText(context,"Created User: ${it.data}", Toast.LENGTH_SHORT).show()
+                    loader.dismiss()
+                    Toast.makeText(context, "Created User: ${it.data}", Toast.LENGTH_SHORT).show()
 
                 }
             }
         }
-
     }
-
-
 }
--- app/src/main/java/com/example/foodhub/WelcomeFragment.kt
+++ app/src/main/java/com/example/foodhub/WelcomeFragment.kt
@@ -1,31 +1,14 @@
 package com.example.foodhub
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.foodhub.databinding.FragmentWelcomeBinding
 
 
+class WelcomeFragment : BasteFragment<FragmentWelcomeBinding>
+    (FragmentWelcomeBinding::inflate) {
 
 
-class WelcomeFragment : Fragment() {
-    lateinit var binding: FragmentWelcomeBinding
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
-       setlistener()
-        return binding.root
-
-    }
-
-    private fun setlistener() {
+        override fun setlistener() {
       with(binding){
           loginTV1.setOnClickListener {
               findNavController().navigate(R.id.action_welcomeFragment_to_loginFragment)
@@ -34,6 +17,10 @@ class WelcomeFragment : Fragment() {
               findNavController().navigate(R.id.action_welcomeFragment_to_registrationFragment)
           }
       }
+    }
+
+    override fun allObserver() {
+
     }
 }
 
