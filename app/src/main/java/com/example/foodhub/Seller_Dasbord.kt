8148200c2a63bb215d7981186de535ea0c1f7b23
mdmum <mdmuminkhan84@gmail.com>
2025-04-14 15:06:09 +0600

    xml-digen

--- app/src/main/java/com/example/foodhub/Seller_Dasbord.kt
+++ app/src/main/java/com/example/foodhub/Seller_Dasbord.kt
@@ -1,6 +1,10 @@
 package com.example.foodhub
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -8,14 +12,17 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.foodhub.databinding.ActivitySellerDasbordBinding
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class Seller_Dasbord : AppCompatActivity(){
   private lateinit var binding: ActivitySellerDasbordBinding
           lateinit var navController: NavController
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    @Inject
+    lateinit var mumin: FirebaseAuth
+   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivitySellerDasbordBinding.inflate(layoutInflater)
 
@@ -32,5 +39,30 @@ class Seller_Dasbord : AppCompatActivity(){
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp()|| super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.seller_top_m,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.logout->{
+                mumin.signOut()
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+            R.id.sattings->{
+                Toast.makeText(this,"Satting Claice", Toast.LENGTH_LONG).show()
+            }
+            R.id.report->{
+                Toast.makeText(this,"Repoft Claice", Toast.LENGTH_LONG).show()
+
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
